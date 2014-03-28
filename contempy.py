70a486b7dab49d8efed7d496f6e4417c14a411ae
Stevie Holdway <stevie.holdway@gmail.com>
2014-03-28 13:38:06 +0000

    Improved contempy, adjusted readme.

--- contempy.py
+++ contempy.py
@@ -3,51 +3,80 @@ import os
 import time
 
 class ContempFile():
-    def __init__(self, session_name, filepath):
-        self.filepath = os.path.abspath(filepath)
-        self.session = session_name
-
-    def start(self):
-        with open(self.filepath, 'w') as file:
-            file.write(self.generate_start_block())
-
-    def end(self):
-        with open(self.filepath, 'a') as file:
-            file.write(self.generate_end_block())
-
-    def generate_start_block(self):
-        output = "SESSION: " + self.session + " | "
-        output = output + "SESSION STARTED: " + time.strftime("%c")
-        output = output + "\n"
-        return output
-
-    def generate_end_block(self):
-        output = "\n\n\n"
-        output = output + "SESSION: " +  self.session + " | "
-        output = output + "SESSION ENDED: " + time.strftime("%c")
-        return output
-
-    def add_item(self, item_text):
-        with open(self.filepath, 'a') as file:
-            message = "\n\n"
-            message = message + "[" + time.strftime("%c") + "]\n"
-            message = message + item_text
-            file.write(message)
-
-
+    def __init__(self, file_path, starts_at, initials):
+        self.filepath = os.path.abspath(file_path)
+        self.starts_at = int(starts_at)    # The note increment
+        self.initials = initials
+        
+    def init_file(self):
+        with open(self.filepath, 'a') as open_file:
+            open_file.write(self.create_start_session())
+    
+    def finish_file(self):
+        with open(self.filepath, 'a') as open_file:
+            open_file.write(self.create_end_session())        
+    
+    def add_note(self, evidence_id, message):
+        number = "N%.3d" % (self.starts_at)
+        self.starts_at += 1
+        
+        first_line = "[{0}: {1}: {2}]".format(number, self.initials, evidence_id)
+        second_line = message + "\n\n"
+        
+        output = first_line + "\n" + second_line
+        
+        with open(self.filepath, 'a') as open_file:
+            open_file.write(output)
+    
+    def create_date_string(self):
+        """
+        Must have the format 2010-04-12 22:15
+        """
+        now = time.localtime()
+        return time.strftime("%Y-%m-%d %H:%M", now)
+    
+    def create_start_session(self):
+        """
+        Must have format [SESSION START: 2010-04-12 22:15]
+        """
+        now = self.create_date_string()
+        return "[SESSION START: " + now + "]\n"
+    
+    def create_end_session(self):
+        """
+        Must have format [SESSION END: 2010-04-12 22:15]
+        """
+        now = self.create_date_string()
+        return "[SESSION END: " + now + "]\n"  
+    
+    
+    
 if __name__=='__main__':
-    print "Welecome to contempy.py!\n"
-    session_name = raw_input("Enter the name for this notes session > ")
-    filepath = raw_input("Enter the filename for the notes file > ")
+    print "Welcome to contempy.py!"
+    filename = raw_input("Contemporaneous file location > ")
+    starts_at = raw_input("What message number should we start at? > ")
+    initials = raw_input("Please enter your initials > ")
+    
+    print "Prompt is starting. Enter ==EXIT== at the message prompt to close the session."
+    
+    # Main loop
+    file = ContempFile(filename, starts_at, initials)
+    file.init_file()
+    while True:
+        evidence_ref = raw_input("Evidence ID > ")
+        message = raw_input("Message > ")
 
-    print "Note taking has begun! Enter your note at the prompt and press enter to commit it. Type '==EXIT==' when you're finished."
+        if evidence_ref == "==EXIT==" or message == "==EXIT==":
+            print "Exiting. Thank you!"
+            break
 
-    note = ""
-    file = ContempFile(session_name, filepath)
-    file.start()
-    while note != "==EXIT==":
-        note = raw_input("> ")
-        if note != '==EXIT==':
-            file.add_item(note)
-
-    file.end()
+        print "Are you sure you want to commit:"
+        print "Evidence Reference: " + evidence_ref
+        print "Message: " + message
+        should_commit = raw_input("YES or NO? ")
+        
+        if should_commit == "YES":
+            file.add_note(evidence_ref, message)
+        
+    file.finish_file()
+    print "Done! Thank you!"
--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
 # contempy.py
 
-A python script for writing contemporaneous notes.
+A python script for writing contemporaneous notes. It adheres to the following format:
+
+```
+[SESSION START: 2014-03-28 13:30]
+[N001: SH: pc3:/etc/init.d]
+There was something wrong with init.d
+
+[SESSION END: 2014-03-28 13:31]
+[SESSION START: 2014-03-28 13:34]
+[N002: SH: pc4:/dev/sda]
+The disk is actually a porcupine.
+
+[SESSION END: 2014-03-28 13:34]
+```
